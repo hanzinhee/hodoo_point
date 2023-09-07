846fa115579f494638ee79f3f556c200eae1c93e
jinhee <32665852+hanzinhee@users.noreply.github.com>
2023-09-07 12:21:24 +0900

    로그인 스크린: 소셜 로그인 버튼

--- lib/features/authentication/views/login_screen.dart
+++ lib/features/authentication/views/login_screen.dart
@@ -1,29 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hodoo_point/constants/gaps.dart';
 import 'package:hodoo_point/features/authentication/repos/authentication_repo.dart';
 import 'package:hodoo_point/widgets/hodoo_app_bar.dart';
 
-class LoginScreen extends ConsumerWidget {
+class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
-
   static const String routePath = '/login';
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends ConsumerState<LoginScreen> {
+  bool checkedKeepLogin = false;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: HodooAppBar(
-        actions: [
-          TextButton(
-            style: TextButton.styleFrom(),
-            child: const Text('회원가입', style: TextStyle(fontSize: 12)),
-            onPressed: () {
-              context.pop();
-            },
-          ),
-        ],
-      ),
+      appBar: HodooAppBar(),
       body: Padding(
         padding: const EdgeInsets.all(Gaps.size2),
         child: Column(
@@ -52,11 +49,15 @@ class LoginScreen extends ConsumerWidget {
                 Row(
                   children: [
                     Checkbox(
-                      value: false,
+                      value: checkedKeepLogin,
                       checkColor: Colors.blueAccent,
                       activeColor: Colors.transparent,
                       side: BorderSide(color: Colors.grey[400]!),
-                      onChanged: (value) {},
+                      onChanged: (value) {
+                        setState(() {
+                          checkedKeepLogin = value!;
+                        });
+                      },
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10),
                           side: BorderSide(color: Colors.grey[400]!)),
@@ -82,6 +83,54 @@ class LoginScreen extends ConsumerWidget {
                 style: ElevatedButton.styleFrom(
                     minimumSize: const Size(double.infinity, 50)),
                 child: const Text('로그인', style: TextStyle(fontSize: 20))),
+            Gaps.v3,
+            const Row(
+              children: [
+                Expanded(
+                    child: Divider(
+                  endIndent: 10,
+                )),
+                Text('간편 로그인', style: TextStyle(color: Colors.grey)),
+                Expanded(child: Divider(indent: 10)),
+              ],
+            ),
+            Gaps.v3,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ClipOval(
+                    child: Image.asset(
+                  'assets/images/icons/logins/apple.png',
+                  width: 44,
+                )),
+                ClipOval(
+                    child: Image.asset(
+                  'assets/images/icons/logins/kakao.png',
+                  width: 44,
+                )),
+                ClipOval(
+                    child: Image.asset(
+                  'assets/images/icons/logins/naver.png',
+                  width: 44,
+                )),
+              ],
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('아직 회원이 아니신가요?',
+                    style: TextStyle(color: Colors.grey)),
+                TextButton(
+                    onPressed: () {
+                      context.go('/signup');
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: const Text('회원가입'))
+              ],
+            )
           ],
         ),
       ),
